911084ae8870f66c2563f93bea6fb6e7401e7c48
xgukar29 <xgukar29@gmail.com>
2023-04-17 20:51:30 +0200

    Trying to add a overview page

--- appNavigator/Navigator.jsx
+++ appNavigator/Navigator.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Calendar from '../screens/CalendarScreen';
+import BookedTimes from '../screens/BookedTimes';
 
 const Stack = createStackNavigator();
 
@@ -18,6 +19,12 @@ const Navigator = () => {
         component={Calendar}
         options={{ title: 'Add laundry' }}
       />
+      <Stack.Screen
+        name="BookedTimes"
+        component={Overview}
+        options={{ title: 'Overview' }}
+      />
+
     </Stack.Navigator>
   );
 
