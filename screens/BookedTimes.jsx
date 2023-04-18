ebc82bd89a7a1d570ab66df7edf381ce322b3520
xgukar29 <xgukar29@gmail.com>
2023-04-18 09:46:55 +0200

    Another try

--- screens/BookedTimes.jsx
+++ screens/BookedTimes.jsx
@@ -1,40 +1,91 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, FlatList } from 'react-native';
-import { collection, query, orderBy, getDocs } from "firebase/firestore"; 
+import React, { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import Calendar from '../components/Calendar';
+import TimePicker from '../components/TimePicker';
+import SaveButton from '../components/SaveButton';
+import { collection, addDoc } from "firebase/firestore"; 
 import db from '../config';
 
-const BookingOverviewScreen = () => {
-  const [bookings, setBookings] = useState([]);
+const BookingTimesScreen = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date());
+  const [bookingTimes, setBookingTimes] = useState([]);
 
-  useEffect(() => {
-    const fetchBookings = async () => {
-      const q = query(collection(db, "time"), orderBy("date"));
-      const querySnapshot = await getDocs(q);
-      const docs = [];
-      querySnapshot.forEach((doc) => {
-        docs.push(doc.data());
+  const handleSelectDate = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleSelectStartTime = (time) => {
+    setStartTime(time);
+  };
+
+  const handleSelectEndTime = (time) => {
+    setEndTime(time);
+  };
+
+  const saveBookingTime = async () => {
+    console.log(`Selected Date: ${selectedDate}`);
+    console.log(`Start Time: ${startTime.toLocaleTimeString()}`);
+    console.log(`End Time: ${endTime.toLocaleTimeString()}`);
+
+    try {
+      const docRef = await addDoc(collection(db, "bookingTimes"), {
+        date: selectedDate,
+        startTime: startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+        endTime: endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
       });
-      setBookings(docs);
-    };
-    fetchBookings();
-  }, []);
+      console.log("Document written with ID: ", docRef.id);
 
-  const renderBookingItem = ({ item }) => {
-    return (
-      <View style={styles.bookingItem}>
-        <Text style={styles.bookingDate}>{item.date}</Text>
-        <Text style={styles.bookingTime}>{item.startTime} - {item.endTime}</Text>
-      </View>
-    );
+      setBookingTimes([
+        ...bookingTimes,
+        { date: selectedDate, startTime, endTime }
+      ]);
+      
+      setSelectedDate(null);
+      setStartTime(new Date());
+      setEndTime(new Date());
+
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
   };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={bookings}
-        renderItem={renderBookingItem}
-        keyExtractor={(item) => item.date}
-      />
+      <View style={styles.calendarContainer}>
+        <Calendar onSelectDate={handleSelectDate} />
+      </View>
+      <View style={styles.timePickersContainer}>
+        <View style={styles.timePickerWrapper}>
+          <Text style={styles.timePickerLabel}>Start Time</Text>
+          <View style={styles.timePickerContainer}>
+            <TimePicker onChange={handleSelectStartTime} date={startTime} />
+          </View>
+        </View>
+        <View style={{ width: 80 }} />
+        <View style={styles.timePickerWrapper}>
+          <Text style={styles.timePickerLabel}>End Time</Text>
+          <View style={styles.timePickerContainer}>
+            <TimePicker onChange={handleSelectEndTime} date={endTime} />
+          </View>
+        </View>
+      </View>
+      <View style={styles.saveButton}>
+        <SaveButton onPress={saveBookingTime} />
+      </View>
+      <View style={styles.bookingTimesContainer}>
+        {bookingTimes.map((bookingTime, index) => (
+          <View key={index} style={styles.bookingTime}>
+            <Text style={styles.bookingTimeText}>
+              {bookingTime.date.toDateString()}
+            </Text>
+            <Text style={styles.bookingTimeText}>
+              {bookingTime.startTime.toLocaleTimeString()} - {bookingTime.endTime.toLocaleTimeString()}
+            </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
@@ -43,21 +94,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-    padding: 20,
   },
-  bookingItem: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-    paddingVertical: 10,
+  calendarContainer: {
+    flex: 1,
   },
-  bookingDate: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  bookingTime: {
-    fontSize: 16,
-  },
-});
-
-export default BookingOverviewScreen;
+  timePickersContainer: {
+    flexDirection: 'row',
+    alignItems: '
