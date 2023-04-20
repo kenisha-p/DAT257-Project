0c31a4bab1761ae8281adc37e4a4a8246fa8a7fc
LinaEkmark <94972956+LinaEkmark@users.noreply.github.com>
2023-04-20 02:48:34 +0000

    Commit message

--- screens/CalendarScreen.jsx
+++ screens/CalendarScreen.jsx
@@ -43,28 +43,64 @@ const CalendarScreen = () => {
     }
   };
 
-//Get from API. 
-async function fetchElectricityPrices() {
-  try {
-    const today = new Date();
+  //this function modifies the data from the api in a usable way
+  function getSEKValues(inputData) {
     
-    // formattedDate gets put into the 'axios.get' request to retrieve the current day's electricity prices 
-    const formattedDate = `${today.getFullYear()}/${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}`;
+    // Create an empty dictionary to hold the results
+    let outputDict = {};
     
-    // Send a GET request to the API endpoint with the formatted date
-    const response = await axios.get(`https://www.elprisetjustnu.se/api/v1/prices/${formattedDate}_SE3.json`);
+    // Loop through the input data and add each date/SEK value pair to the output dictionary
+    inputData.forEach(data => {
+      const date = data.time_start.split('T')[0]; // Extract the date portion of the timestamp
+      
+      const time = data.time_start.split('T')[1]; // Extract the time portion of the timestamp
+      const timeSlice = time.slice(0, 5); // Extract the first four characters, or just the hours
+
+      datetime = date + " / " + timeSlice;
+
+      const SEKvalue = data.SEK_per_kWh;
+      
+      outputDict[datetime] = SEKvalue; // Use the date as the key
+    });
     
-    //At this stage, data from API only is printed in console.
-    console.log(response.data);
-  } catch (error) {
-    console.error('Error fetching electricity prices: ', error);
+    // Return the output dictionary
+    return outputDict;
   }
-}
+  
+  async function asynchronousFunction() {
+    try {
+      const today = new Date();
 
-  //Calls the function which displays the API
-  fetchElectricityPrices();
+      // formattedDate gets put into the 'axios.get' request to retrieve the current day's electricity prices 
+      const formattedDate = `${today.getFullYear()}/${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}`;
 
+      // Send a GET request to the API endpoint with the formatted date
+      const response = await axios.get(`https://www.elprisetjustnu.se/api/v1/prices/${formattedDate}_SE3.json`);
 
+      console.log("under fetch");
+      console.log(response)
+      return response;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  function fetchElectricityPrices() {
+    let SEKValuesFinal = {}; // declare SEKValuesFinal with let keyword
+    const response = asynchronousFunction();
+
+    console.log("efter fetch");
+    console.log(response.data);
+    //At this stage, data from API only is printed in console.
+    SEKValuesFinal = getSEKValues(response.data);   
+    return SEKValuesFinal; // return the value from the function
+  }
+
+  const sekValuesFinal = fetchElectricityPrices();
+
+  console.log ("SEKvaluesfinal");
+  console.log (SEKvaluesfinal);
+  
   
 //Render for the UI or "View". Handles the textbars for "Time" and "Total cost". Handles as well the (sort of) 
 //radio buttons which the user is to press for booking. 
@@ -73,18 +109,35 @@ async function fetchElectricityPrices() {
       <View style={styles.calendarContainer}>
         <Calendar onSelectDate={handleSelectDate} />
       </View>
+
+
+      <View style={styles.textContainer}>
+      {Object.keys(sekValuesFinal).map((key) => (
+        <View key={key} style={styles.textItem}>
+          <Text style={{ fontSize: 20 }}>{key}</Text>
+          <Text style={{ fontSize: 16, color: '#888' }}>{sekValuesFinal[key]}</Text>
+        </View>
+      ))}
+    </View>
   
+
       <View style={styles.textContainer}>
         <View style={styles.timeText}>
-          <Text style={{ fontSize: 20 }}>Time</Text>
+          <Text style={{ fontSize: 20 }}>Times</Text>
+          <Text style={{ fontSize: 16, color: '#888' }}>{}</Text>
         </View>
         <View style={styles.priceText}>
           <Text style={{ fontSize: 20 }}>Total cost</Text>
+          <Text style={{ fontSize: 16, color: '#888' }}>Enter the total cost:</Text>
         </View>
         <View style={styles.bookText}>
           <Text style={{ fontSize: 20 }}>Book</Text>
+          <Text style={{ fontSize: 16, color: '#888' }}>Click to book:</Text>
         </View>
       </View>
+
+      
+
   
       <View style={styles.timeSlotsContainer}>
         {timeSlots.map((timeSlot, index) => (
